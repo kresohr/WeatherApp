e32c24fa987c5aaf4c9f21001bff96af8795a3ec
kresohr <48351282+kresohr@users.noreply.github.com>
2022-11-16 21:54:27 +0100

    Repository: Method to fetch remote data added

--- app/src/main/java/com/ikresimir/weatherilicic/source/Repository.kt
+++ app/src/main/java/com/ikresimir/weatherilicic/source/Repository.kt
@@ -1,22 +1,31 @@
 package com.ikresimir.weatherilicic.source
 
+import androidx.lifecycle.MutableLiveData
+import com.ikresimir.weatherilicic.model.Location
+import com.ikresimir.weatherilicic.source.remote.OpenWeatherAPIClient
+import com.ikresimir.weatherilicic.source.remote.OpenWeatherAPIInterface
+
+/*
+     API_KEY would usually be hidden from VCS by using "Gradle Secret Plugin" or other methods.
+     This is just for task demonstration purpose.
+*/
+const val API_KEY="318ce672275d66205dd441034c200da6"
+object Repository {
 
 
-class Repository {
-
-    /* API_KEY would usually be hidden from VCS by using "Gradle Secret Plugin" or other methods.
-     This is just for task demonstration purpose.*/
-    val API_KEY="318ce672275d66205dd441034c200da6"
-
-    fun getRemoteData(){
-
+    //TO DO: Create dynamic call with arguments on API.
+    //Location will be retrieved from user through permission
+    suspend fun getRemoteData(location: MutableLiveData<Location>) {
+        val apiClient = OpenWeatherAPIClient.getInstance().create(OpenWeatherAPIInterface::class.java)
+        val result = apiClient.getCurrentWeather("onecall?lat=45.8426414&lon=15.9622315&exclude=minutely,alerts&units=metric&appid="+ API_KEY)
+        location.value = result
     }
 
     fun saveToLocalDatabase(){}
 
     fun getLocalData(){}
 
-    // This function will compare local and remote data, if same,
+    // This function will compare local and remote data, if same, fetch local
     fun compareData(){
 
     }
--- app/src/main/java/com/ikresimir/weatherilicic/source/remote/OpenWeatherAPIClient.kt
+++ app/src/main/java/com/ikresimir/weatherilicic/source/remote/OpenWeatherAPIClient.kt
@@ -5,18 +5,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object OpenWeatherAPIClient {
 
-    // https://api.openweathermap.org/data/3.0/onecall?lat=45.815010&lon=15.981919&exclude=minutely,hourly.pressure,alerts,hourly.pressure,hourly.humidity,hourly.dew_point,hourly.uvi,hourly.clouds,hourly.visibility,hourly.wind_deg,hourly.wind_gust,weather.main,weather.description,weather.icon&units=metric&appid=318ce672275d66205dd441034c200da6
-
     fun getInstance(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://api.openweathermap.org/data/3.0/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
-
-
-
-
 
 
 
