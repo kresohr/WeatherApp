cfd9f99a6674763df4d42b6678631a95f2d883cd
kresohr <48351282+kresohr@users.noreply.github.com>
2022-11-16 21:57:15 +0100

    MainViewModel: Retrieve data from Repository and save in LiveData, method to check if night time added

--- app/src/main/java/com/ikresimir/weatherilicic/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ikresimir/weatherilicic/viewmodel/MainViewModel.kt
@@ -1,8 +1,37 @@
 package com.ikresimir.weatherilicic.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ikresimir.weatherilicic.model.Location
+import com.ikresimir.weatherilicic.source.Repository
+import kotlinx.coroutines.launch
+import java.util.*
 
 class MainViewModel : ViewModel() {
 
+    private val weatherInUserLocation: MutableLiveData<Location> by lazy{
+        MutableLiveData<Location>()
+    }
 
+    fun getWeather(): MutableLiveData<Location>{
+        viewModelScope.launch {
+            try{
+                Repository.getRemoteData(weatherInUserLocation)
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+        return weatherInUserLocation
+    }
+
+    // Can be improved to check based on the season (Daylight is prone to change based on season)
+    fun checkIfNight(): Boolean{
+        val test = weatherInUserLocation.value?.current?.dt
+        val sdf= java.text.SimpleDateFormat("HH")
+        val date = Date((test?.times(1000L)!!))
+        val hour = sdf.format(date).toInt()
+
+        return hour>16 || hour<7
+    }
 }
