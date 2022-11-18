0aceaa412037f0eb5661177d0fe839522e44500f
kresohr <48351282+kresohr@users.noreply.github.com>
2022-11-18 10:11:22 +0100

    build.gradle: Set data & viewbinding to true, MainActivity: Switched to viewBinding, Activity_forecast.xml: Added <layout> tag, activity_main.xml: Added <layout> tag

--- app/src/main/java/com/ikresimir/weatherilicic/view/MainActivity.kt
+++ app/src/main/java/com/ikresimir/weatherilicic/view/MainActivity.kt
@@ -2,47 +2,29 @@ package com.ikresimir.weatherilicic.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.appcompat.widget.SearchView
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.ikresimir.weatherilicic.R
+import com.ikresimir.weatherilicic.databinding.ActivityMainBinding
+
 import com.ikresimir.weatherilicic.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var btnShowForecast: Button
-    lateinit var textMinValue: TextView
-    lateinit var textMaxValue: TextView
-    lateinit var textDegrees: TextView
-    lateinit var textCity: TextView
-    lateinit var textSkyState: TextView
-    lateinit var imageWeather: ImageView
-    lateinit var searchCity: SearchView
     lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //TO DO: Add string resources
-
-        //Can be improved with viewBinding
-        btnShowForecast = findViewById(R.id.btnShowForecast)
-        textMinValue = findViewById(R.id.text_min_value)
-        textMaxValue = findViewById(R.id.text_max_value)
-        textCity = findViewById(R.id.text_city)
-        textDegrees = findViewById(R.id.text_degrees)
-        textSkyState = findViewById(R.id.text_sky_state)
-        imageWeather = findViewById(R.id.image_weather)
-        searchCity = findViewById(R.id.search)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.getWeather().observe(this) {
-            textDegrees.text = it.current.temp.toInt().toString() + " °C"
-            textMinValue.text = it.daily[0].temp.min.toInt().toString() + " °C"
-            textMaxValue.text = it.daily[0].temp.max.toInt().toString() + " °C"
-            textSkyState.text = it.current.weather[0].main
-            textCity.text = viewModel.getCityName()
+            binding.textDegrees.text = it.current.temp.toInt().toString() + " °C"
+            binding.textMinValue.text = it.daily[0].temp.min.toInt().toString() + " °C"
+            binding.textMaxValue.text = it.daily[0].temp.max.toInt().toString() + " °C"
+            binding.textSkyState.text = it.current.weather[0].main
+            binding.textCity.text = viewModel.getCityName()
             setCurrentWeatherImage(it.current.weather[0].main)
         }
     }
@@ -52,20 +34,20 @@ class MainActivity : AppCompatActivity() {
         //TO DO: Add more weather modes "Fog, Sandstorm... etc"
         if(isNight){
             when(weatherDescription){
-                "Thunderstorm" -> imageWeather.setImageResource(R.drawable.ic_thunder_night)
-                "Rain" -> imageWeather.setImageResource(R.drawable.ic_rain_night)
-                "Snow" -> imageWeather.setImageResource(R.drawable.ic_snow_night)
-                "Clear" -> imageWeather.setImageResource(R.drawable.ic_clear_night)
-                "Clouds" -> imageWeather.setImageResource(R.drawable.ic_cloud_night)
+                "Thunderstorm" -> binding.imageWeather.setImageResource(R.drawable.ic_thunder_night)
+                "Rain" -> binding.imageWeather.setImageResource(R.drawable.ic_rain_night)
+                "Snow" -> binding.imageWeather.setImageResource(R.drawable.ic_snow_night)
+                "Clear" -> binding.imageWeather.setImageResource(R.drawable.ic_clear_night)
+                "Clouds" -> binding.imageWeather.setImageResource(R.drawable.ic_cloud_night)
             }
         }
         else{
             when (weatherDescription){
-                "Thunderstorm" -> imageWeather.setImageResource(R.drawable.ic_thunder)
-                "Rain" -> imageWeather.setImageResource(R.drawable.ic_rain)
-                "Snow" -> imageWeather.setImageResource(R.drawable.ic_snow)
-                "Clear" -> imageWeather.setImageResource(R.drawable.ic_sun)
-                "Clouds" -> imageWeather.setImageResource(R.drawable.ic_cloud)
+                "Thunderstorm" -> binding.imageWeather.setImageResource(R.drawable.ic_thunder)
+                "Rain" -> binding.imageWeather.setImageResource(R.drawable.ic_rain)
+                "Snow" -> binding.imageWeather.setImageResource(R.drawable.ic_snow)
+                "Clear" -> binding.imageWeather.setImageResource(R.drawable.ic_sun)
+                "Clouds" -> binding.imageWeather.setImageResource(R.drawable.ic_cloud)
             }
         }
 
